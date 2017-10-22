734da15079a54827573c1366abef3af0b76989e3
samiuddin <sami.md@hotmail.com>
2017-10-22 14:38:11 +0530

    removed redundant items

--- app.js
+++ app.js
@@ -50,21 +50,7 @@ var server = http.createServer(function(request,response) {
                 response.writeHead(200, { 'Content-Type': contentType });
                 response.end(content, 'utf-8');
             }
-        });
-
-        // Website you wish to allow to connect
-        response.setHeader('Access-Control-Allow-Origin', '*');
-
-        // Request methods you wish to allow
-        response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-        // Request headers you wish to allow
-        response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        response.setHeader('Access-Control-Allow-Credentials', true);
-    
+        });    
 });
 
 server.listen(3000);
